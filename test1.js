2bdc25107176d972c6d9df2fe18b2f76476e9c13
Najam Us Saqib Khan <61153309+najam1997@users.noreply.github.com>
2024-05-25 11:57:20 +0000

    Encoding and Decoding both keys

--- test1.js
+++ test1.js
@@ -1,17 +1,25 @@
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
 
-const str_private = fs.readFileSync('/workspaces/Deliberately_Vulnerable_JWT/private.pem', 'utf8');
-const publicKey = fs.readFileSync('/workspaces/Deliberately_Vulnerable_JWT/public.pem', 'utf8');
+const str_privateKey = fs.readFileSync('/workspaces/Deliberately_Vulnerable_JWT/private.pem', 'utf8');
+const str_publicKey = fs.readFileSync('/workspaces/Deliberately_Vulnerable_JWT/public.pem', 'utf8');
 
+// Base64 encoding Public Key
+const buffer1 = Buffer.from(str_publicKey);
+const enc_publicKey = buffer1.toString('base64');
+console.log(enc_publicKey); 
 
-// Base64 encoding
+// Base64 decoding Public Key
+const dec_publicKey = Buffer.from(enc_publicKey, 'base64');
+const publicKey = dec_publicKey.toString();
+console.log(publicKey);
 
-const buffer = Buffer.from(str_private);
-const enc_privateKey = buffer.toString('base64');
+// Base64 encoding Private Key
+const buffer2 = Buffer.from(str_privateKey);
+const enc_privateKey = buffer2.toString('base64');
 console.log(enc_privateKey); 
 
-// Base64 decoding
+// Base64 decoding Private Key
 const dec_privateKey = Buffer.from(enc_privateKey, 'base64');
 const privateKey = dec_privateKey.toString();
 console.log(privateKey);
